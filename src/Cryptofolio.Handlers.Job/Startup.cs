990105159eec83169ac3e3cf6a07e1aa85273569
Fabien PERRONNET <perronnet-fabien@live.fr>
2021-07-13 16:57:59 +1100

    Add events job.

---
+++ src/Cryptofolio.Handlers.Job/Startup.cs
@@ -0,0 +1,73 @@
+using Confluent.Kafka;
+using Cryptofolio.Core;
+using Cryptofolio.Infrastructure;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Internal;
+
+namespace Cryptofolio.Handlers.Job
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // EF Core
+            services.AddDbContext<CryptofolioContext>(builder =>
+            {
+                builder.UseNpgsql(Configuration.GetConnectionString("CryptofolioContext"));
+                if (Environment.IsDevelopment())
+                {
+                    builder.EnableSensitiveDataLogging();
+                    builder.EnableDetailedErrors();
+                }
+            });
+
+            // Kafka
+            services.AddConsumer<IEvent>(options =>
+            {
+                options.Topic = Configuration.GetSection($"Kafka:Topics:{typeof(IEvent).FullName}").Get<string>();
+                options.Config = Configuration.GetSection("Kafka:Consumer").Get<ConsumerConfig>();
+            });
+
+            // MediatR
+            services.AddMediatR(typeof(Startup));
+
+            // Healthchecks
+            services
+                .AddHealthChecks()
+                .AddNpgSql(Configuration.GetConnectionString("CryptofolioContext"), name: "db")
+                .AddKafka(
+                    Configuration.GetSection("Kafka:Producer").Get<ProducerConfig>(),
+                    Configuration.GetSection("Kafka:Topics:HealthChecks").Get<string>()
+                );
+
+            services.TryAddSingleton<ISystemClock, SystemClock>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHealthChecks("/health");
+        }
+    }
+}
