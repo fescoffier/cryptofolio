3fb5de3f72602d22daee7970b983299fc8d775ea
Fabien PERRONNET <perronnet-fabien@live.fr>
2021-08-04 13:12:30 +1100

    Add app.

---
+++ src/Cryptofolio.App/Program.cs
@@ -0,0 +1,50 @@
+using Cryptofolio.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Text.Json;
+
+namespace Cryptofolio.App
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            // The global logger is used only when the host starts and stops.
+            // Otherwise, the logger factory configured in the host is used.
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .Enrich.FromLogContext()
+                .WriteTo.Console(theme: InfrastructureConstants.Logging.Theme, outputTemplate: InfrastructureConstants.Logging.OutputTemplate)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host.");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated unexpectedly.");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(new JsonSerializerOptions
+                {
+                    Converters =
+                    {
+                        new IEventJsonConverter()
+                    }
+                })
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+    }
+}
---
+++ src/Cryptofolio.App/Startup.cs
@@ -0,0 +1,99 @@
+using Confluent.Kafka;
+using Cryptofolio.Infrastructure;
+using Elasticsearch.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
+using System.Linq;
+
+namespace Cryptofolio.App
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Mvc.
+            services.AddControllersWithViews();
+
+            // Elasticsearch
+            services.AddSingleton<IConnectionPool>(
+                new StaticConnectionPool(
+                    Configuration.GetSection("Elasticsearch:Nodes")
+                        .Get<string[]>()
+                        .Select(n => new Node(new(n)))
+                        .ToList()
+                )
+            );
+
+            // Redis
+            services.AddSingleton(ConnectionMultiplexer.Connect(Configuration.GetConnectionString("Redis")));
+            services.AddSingleton<IConnectionMultiplexer>(p => p.GetRequiredService<ConnectionMultiplexer>());
+            services.AddTransient(p => p.GetRequiredService<ConnectionMultiplexer>().GetDatabase());
+
+            // Healthchecks
+            services
+                .AddHealthChecks()
+                .AddKafka(
+                    Configuration.GetSection("Kafka:Producer").Get<ProducerConfig>(),
+                    Configuration.GetSection("Kafka:Topics:HealthChecks").Get<string>(),
+                    name: "kafka"
+                )
+                .AddRedis(Configuration.GetConnectionString("Redis"), name: "redis")
+                .AddCheck<ElasticsearchHealthCheck>("elasticsearch");
+
+            // Angular
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHealthChecks("/health");
+
+            app.UseStaticFiles();
+
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
