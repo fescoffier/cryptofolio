895cc065ec53974228ddb4c9a22ec5645ed2f276
Fabien PERRONNET <perronnet-fabien@live.fr>
2021-08-07 16:09:40 +1100

    Add the template front app.

--- src/Cryptofolio.App/ClientApp/src/app/app-routing.module.ts
+++ src/Cryptofolio.App/ClientApp/src/app/app-routing.module.ts
@@ -1,10 +1,95 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+import { AdminLayoutComponent } from "./layouts/admin-layout/admin-layout.component";
+import { AuthLayoutComponent } from "./layouts/auth-layout/auth-layout.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "dashboard",
+    pathMatch: "full"
+  },
+  {
+    path: "",
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: "",
+        loadChildren:
+          "./pages/examples/dashboard/dashboard.module#DashboardModule"
+      },
+      {
+        path: "components",
+        loadChildren:
+          "./pages/examples/components/components.module#ComponentsPageModule"
+      },
+      {
+        path: "forms",
+        loadChildren: "./pages/examples/forms/forms.module#Forms"
+      },
+      {
+        path: "tables",
+        loadChildren: "./pages/examples/tables/tables.module#TablesModule"
+      },
+      {
+        path: "maps",
+        loadChildren: "./pages/examples/maps/maps.module#MapsModule"
+      },
+      {
+        path: "widgets",
+        loadChildren: "./pages/examples/widgets/widgets.module#WidgetsModule"
+      },
+      {
+        path: "charts",
+        loadChildren: "./pages/examples/charts/charts.module#ChartsModule"
+      },
+      {
+        path: "calendar",
+        loadChildren:
+          "./pages/examples/calendar/calendar.module#CalendarModulee"
+      },
+      {
+        path: "",
+        loadChildren:
+          "./pages/examples/pages/user/user-profile.module#UserModule"
+      },
+      {
+        path: "",
+        loadChildren:
+          "./pages/examples/pages/timeline/timeline.module#TimelineModule"
+      }
+    ]
+  },
+  {
+    path: "",
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: "pages",
+        loadChildren: "./pages/examples/pages/pages.module#PagesModule"
+      }
+    ]
+  },
+  {
+    path: "**",
+    redirectTo: "dashboard"
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      scrollPositionRestoration: "enabled",
+      anchorScrolling: "enabled",
+      scrollOffset: [0, 64]
+    })
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/Cryptofolio.App/ClientApp/src/app/app.module.ts
+++ src/Cryptofolio.App/ClientApp/src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ToastrModule } from "ngx-toastr";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { AdminLayoutComponent } from "./layouts/admin-layout/admin-layout.component";
+import { AuthLayoutComponent } from "./layouts/auth-layout/auth-layout.component";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { ComponentsModule } from "./components/components.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLayoutComponent,
+    AuthLayoutComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    AppRoutingModule,
+    ToastrModule.forRoot(),
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
