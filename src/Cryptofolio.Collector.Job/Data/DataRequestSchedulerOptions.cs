52cbe20cd57f6ed4b5c4e2cc4be3d42bbd2858ff
Fabien PERRONNET <perronnet-fabien@live.fr>
2021-08-15 13:11:29 +1100

    Ensure that schedulers do not overlap when they schedule new data request. (#11)

--- src/Cryptofolio.Collector.Job/Data/DataRequestSchedulerOptions.cs
+++ src/Cryptofolio.Collector.Job/Data/DataRequestSchedulerOptions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Cryptofolio.Collector.Job.Data
@@ -13,13 +14,33 @@ namespace Cryptofolio.Collector.Job.Data
         public int CronCheckIntervalMs { get; set; }
 
         /// <summary>
-        /// The interval in milliseconds between cron checks of the schedule while it's invalid.
-        /// </summary>
-        public int InvalidCronCheckIntervalMs { get; set; }
-
-        /// <summary>
         /// The schedules.
         /// </summary>
         public Dictionary<string, string> Schedules { get; set; }
+
+        /// <summary>
+        /// The Redis key to store the schedulers list.
+        /// </summary>
+        public string SchedulersLockKey { get; set; }
+
+        /// <summary>
+        /// The timespan to set the schedulers lock expiration.
+        /// </summary>
+        public TimeSpan SchedulersLockExpiration { get; set; }
+
+        /// <summary>
+        /// The timespan that defines the interval between two schedulers lock checks.
+        /// </summary>
+        public TimeSpan SchedulersLockCheckInterval { get; set; }
+
+        /// <summary>
+        /// The Redis key to store the schedulers list.
+        /// </summary>
+        public string SchedulersHashKey { get; set; }
+
+        /// <summary>
+        /// The Redis key format to store the schedules hash.
+        /// </summary>
+        public string SchedulesHashKeyFormat { get; set; }
     }
 }
